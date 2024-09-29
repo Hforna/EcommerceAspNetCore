b80d54e7d5ced1d8b1036757868be849fc3e7d75
Hforna <henriqueflashpingo@gmail.com>
2024-09-29 10:15:04 -0300

    add errors filter

---
+++ src/backend/EcommerceAspNet.Api/Filters/FilterException.cs
@@ -0,0 +1,18 @@
+﻿using EcommerceAspNet.Exception.Exception;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace EcommerceAspNet.Api.Filters
+{
+    public class FilterException : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            if(context.Exception is BaseException baseException)
+            {
+                context.HttpContext.Response.StatusCode = (int)baseException.GetStatusCode();
+                context.Result = new BadRequestObjectResult(new ResponseErrorJson(baseException.GetErrorMessage()));
+            }
+        }
+    }
+}
--- src/backend/EcommerceAspNet.Api/Program.cs
+++ src/backend/EcommerceAspNet.Api/Program.cs
@@ -12,6 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddMvc(opt => opt.Filters.Add(typeof(FilterException)));
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
