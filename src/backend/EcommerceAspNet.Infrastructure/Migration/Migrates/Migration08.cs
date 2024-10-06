0d62c32207ad92f0f0cfb194b5a9eeaf8dde6707
Hforna <henriqueflashpingo@gmail.com>
2024-10-06 06:09:13 -0300

    create image for user and comments table

---
+++ src/backend/EcommerceAspNet.Infrastructure/Migration/Migrates/Migration08.cs
@@ -0,0 +1,25 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceAspNet.Infrastructure.Migration.Migrates
+{
+    [Migration(8, "create comments table and user image")]
+    public class Migration08 : BaseMigration
+    {
+        public override void Up()
+        {
+            CreateTable("comments")
+                .WithColumn("Text").AsString()
+                .WithColumn("Note").AsInt32()
+                .WithColumn("UserId").AsInt64().ForeignKey("FK_COMMENT_USER", "users", "Id").OnDelete(System.Data.Rule.Cascade)
+                .WithColumn("ProductId").AsInt64().ForeignKey("FK_COMMENT_PRODUCT", "products", "Id").OnDelete(System.Data.Rule.Cascade);
+
+            Alter.Table("users")
+                .AddColumn("ImageIdentifier").AsString().Nullable();
+        }
+    }
+}
