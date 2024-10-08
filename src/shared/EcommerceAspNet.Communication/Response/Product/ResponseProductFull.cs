5518f987d6276b78cde0f9dfd256e4a2451e827e
Hforna <henriqueflashpingo@gmail.com>
2024-10-08 04:42:53 -0300

    fix response product

--- src/shared/EcommerceAspNet.Communication/Response/Product/ResponseProductFull.cs
+++ src/shared/EcommerceAspNet.Communication/Response/Product/ResponseProductFull.cs
@@ -9,7 +9,7 @@ namespace EcommerceAspNet.Communication.Response.Product
 {
     public class ResponseProductFull
     {
-        public string Text { get; set; }
+        public string Name { get; set; }
         public string Description { get; set; }
         public float Price { get; set; }
         public long Stock { get; set; }
