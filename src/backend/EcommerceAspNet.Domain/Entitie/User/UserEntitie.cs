1f45c7eab23da4125360c39ba25a3937ecfb426f
Hforna <henriqueflashpingo@gmail.com>
2024-10-19 00:47:02 -0300

    fix date time now on user's CreatedOn

--- src/backend/EcommerceAspNet.Domain/Entitie/User/UserEntitie.cs
+++ src/backend/EcommerceAspNet.Domain/Entitie/User/UserEntitie.cs
@@ -10,7 +10,7 @@ namespace EcommerceAspNet.Domain.Entitie.User
     public class UserEntitie : IdentityUser<long>
     {
         public bool Active { get; set; } = true;
-        public DateTime CreatedOn {  get; set; }
+        public DateTime CreatedOn {  get; set; } = DateTime.UtcNow;
         public string Password { get; set; } = string.Empty;
         public string? ImageIdentifier { get; set; }
         public Guid UserIdentifier { get; set; } = Guid.NewGuid();
