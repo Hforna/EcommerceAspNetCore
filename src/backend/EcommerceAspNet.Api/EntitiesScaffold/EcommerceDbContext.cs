fb1b879013c914fb4ac6001bf48db8d940b87406
Hforna <henriqueflashpingo@gmail.com>
2024-10-30 23:03:57 -0300

    reverse scaffold with entity framework

---
+++ src/backend/EcommerceAspNet.Api/EntitiesScaffold/EcommerceDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceAspNet.Api.EntitiesScaffold;
+
+public partial class EcommerceDbContext : DbContext
+{
+    public EcommerceDbContext()
+    {
+    }
+
+    public EcommerceDbContext(DbContextOptions<EcommerceDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
