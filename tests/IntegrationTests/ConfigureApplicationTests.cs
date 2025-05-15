2697fb680dc21b806f716660846121ce453be071
Hforna <henriqueflashpingo@gmail.com>
2025-05-15 18:24:59 -0300

    configure integration tests

---
+++ tests/IntegrationTests/ConfigureApplicationTests.cs
@@ -0,0 +1,99 @@
+﻿using EcommerceAspNet.Communication.Response.User;
+using EcommerceAspNet.Domain.Entitie.Identity;
+using EcommerceAspNet.Domain.Entitie.User;
+using EcommerceAspNet.Domain.Repository;
+using EcommerceAspNet.Domain.Repository.Security;
+using EcommerceAspNet.Domain.Repository.Security.Cryptography;
+using EcommerceAspNet.Domain.Repository.User;
+using EcommerceAspNet.Exception.Exception;
+using EcommerceAspNet.Infrastructure.DataEntity;
+using Microsoft.AspNetCore.Authentication.BearerToken;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace IntegrationTests
+{
+    public class ConfigureApplicationTests : WebApplicationFactory<Program>
+    {
+        public ProjectDbContext DbContext;
+        private readonly IServiceProvider _serviceProvider;
+
+        public ConfigureApplicationTests()
+        {
+            _serviceProvider = Services;
+            var scope = _serviceProvider.CreateScope();
+            DbContext = scope.ServiceProvider.GetRequiredService<ProjectDbContext>();
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(async service =>
+            {
+                var verifyDbContext = service.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<ProjectDbContext>));
+
+                if (verifyDbContext is not null)
+                    service.Remove(verifyDbContext);
+
+                service.AddDbContext<ProjectDbContext>(d =>
+                {
+                    d.UseInMemoryDatabase("InMemoryDbForTesting");
+                });
+
+                using var scope = service.BuildServiceProvider().CreateScope();
+                var uof = scope.ServiceProvider.GetRequiredService<IUnitOfWork>();
+                var userWrite = scope.ServiceProvider.GetRequiredService<IUserWriteOnlyRepository>();
+                var crypt = scope.ServiceProvider.GetRequiredService<IPasswordCryptography>();
+                var userS = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+                var roles = scope.ServiceProvider.GetRequiredService<RoleManager<RoleEntitie>>();
+                await roles.CreateAsync(new RoleEntitie("admin"));
+                await roles.CreateAsync(new RoleEntitie("customer"));
+                
+                var user = new User()
+                {
+                    Active = true,
+                    Email = "testemail@gmail.com",
+                    Password = crypt.Encrypt("testpassword"),
+                    EmailConfirmed = true,
+                    UserName = "testuser",
+                    UserIdentifier = Guid.NewGuid(),
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    ConcurrencyStamp = Guid.NewGuid().ToString()
+                };
+                await userWrite.Add(user);
+                await uof.Commit();
+
+                await userS.AddToRoleAsync(user, "customer");
+                
+            });
+            base.ConfigureWebHost(builder);
+        }
+
+        public async Task<HttpClient> GenerateClientWithToken()
+        {
+            var client = this.CreateClient();
+
+            var request = await client.PostAsJsonAsync("api/login", new { email = "testemail@gmail.com", password = "testpassword" });
+
+            var response = await request.Content.ReadAsStringAsync();
+            if(request.IsSuccessStatusCode)
+            {
+                var serializer = JsonSerializer.Deserialize<ResponseCreateUser>(response);
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", serializer.TokenGenerated);
+
+                return client;
+            }
+            throw new UserException(response);
+        }
+    }
+}
---
+++ tests/IntegrationTests/GetProductTests.cs
@@ -0,0 +1,61 @@
+﻿using EcommerceAspNet.Communication.Response.Product;
+using EcommerceAspNet.Domain.Entitie.Ecommerce;
+using Microsoft.AspNetCore.Http;
+using Sqids;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace IntegrationTests
+{
+    public class GetProductTests : IClassFixture<ConfigureApplicationTests>
+    {
+        private readonly ConfigureApplicationTests _app;
+
+        public GetProductTests(ConfigureApplicationTests app) => _app = app;
+
+        [Fact]
+        public async Task Product_doesnt_exists()
+        {
+            var client = await _app.GenerateClientWithToken();
+            var sqids = new SqidsEncoder<long>(new SqidsOptions() { Alphabet = "f78D", MinLength = 4 });
+            
+            var request = await client.GetAsync($"api/product/{sqids.Encode(30)}");
+
+            var response = await request.Content.ReadAsStringAsync();
+
+            Assert.Equal(HttpStatusCode.BadRequest, request.StatusCode);
+            Assert.Contains("This product doesn't exist", response);
+        }
+
+        [Fact]
+        public async Task Product_Success()
+        {
+            var client = await _app.GenerateClientWithToken();
+            var sqids = new SqidsEncoder<long>(new SqidsOptions() { Alphabet = "f78D", MinLength = 4 });
+            var product = new Product()
+            {
+                Id = 18,
+                Active = true,
+                Name = "Iphone 15",
+                Price = 1000,
+                Stock = 2000,
+                CreatedOn = DateTime.UtcNow
+            };
+
+            await _app.DbContext.Products.AddAsync(product);
+            await _app.DbContext.SaveChangesAsync();
+
+            var request = await client.GetAsync($"api/product/{sqids.Encode(product.Id)}");
+
+            var response = await request.Content.ReadAsStreamAsync();
+            var responseAsJson = JsonDocument.Parse(response);
+            Assert.Equal(HttpStatusCode.OK, request.StatusCode);
+            Assert.Equal(product.Name, responseAsJson.RootElement.GetProperty("name").GetString());
+        }
+    }
+}
