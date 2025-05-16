ad95aaf98742ddacb9f654ae596cf9f0a8d97f6a
Hforna <henriqueflashpingo@gmail.com>
2025-05-16 17:33:42 -0300

    tests create tests for products pagination endpoint using bogus as fake generator

---
+++ tests/CommonUtilities/ProductFaker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bogus;
+using EcommerceAspNet.Domain.Entitie.Ecommerce;
+
+namespace CommonUtilities
+{
+    public static class ProductFaker
+    {
+        public static Product GenerateProduct()
+        {
+            return new Faker<Product>()
+                .RuleFor(d => d.Description, f => f.Lorem.Paragraph())
+                .RuleFor(d => d.Name, f => f.Lorem.Sentences())
+                .RuleFor(d => d.Id, 1)
+                .RuleFor(d => d.Stock, 500);
+        }
+
+        public static List<Product> GenerateProducts(int qty)
+        {
+            var products = new List<Product>();
+            for(var i = 1; i <= qty; i++)
+            {
+                products.Add(new Faker<Product>()
+                .RuleFor(d => d.Description, f => f.Lorem.Paragraph())
+                .RuleFor(d => d.Name, f => f.Lorem.Sentences())
+                .RuleFor(d => d.Stock, 500));
+            }
+            return products;
+        }
+    }
+}
--- tests/IntegrationTests/GetProductsTests.cs
+++ tests/IntegrationTests/GetProductsTests.cs
@@ -1,17 +1,55 @@
-﻿using System;
+﻿using CommonUtilities;
+using EcommerceAspNet.Communication.Request.Product;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EcommerceAspNet.Domain.Entitie.Ecommerce;
+using System.Text.Json;
+using EcommerceAspNet.Communication.Response.Product;
 
 namespace IntegrationTests
 {
-    public class GetProductsTests
+    public class GetProductsTests : IClassFixture<ConfigureApplicationTests>
     {
-        //[Fact]
-        //public async Task Page_Out_Range_Error()
-        //{
-        //
-        //}
+        private readonly ConfigureApplicationTests _app;
+
+        public GetProductsTests(ConfigureApplicationTests app) => _app = app;
+
+        [Fact]
+        public async Task Page_Out_Range_No_Content()
+        {
+            //Arrange
+            var client = _app.CreateClient();
+
+            var products = ProductFaker.GenerateProducts(100);
+            await _app.DbContext.Products.AddRangeAsync(products);
+            await _app.DbContext.SaveChangesAsync();
+
+            //Act
+            var response = await client.PostAsJsonAsync("api/product/11", new RequestProducts());
+
+            //Assert
+            Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Success()
+        {
+            var client = _app.CreateClient();
+
+            var products = ProductFaker.GenerateProducts(50);
+            await _app.DbContext.AddRangeAsync(products);
+            await _app.DbContext.SaveChangesAsync();
+
+            var request = await client.PostAsJsonAsync("api/product/5", new RequestProducts());
+            var response = await request.Content.ReadAsStreamAsync();
+            var toJson = JsonDocument.Parse(response);
+            Assert.Equal(10, toJson.RootElement.GetProperty("count").GetInt32());
+        }
     }
 }
